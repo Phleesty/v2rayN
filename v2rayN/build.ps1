eaf9e21097f01fcbe7eccf20afebcb8b9bb41045
Phleesty <phleesty@gmail.com>
2024-11-18 16:47:27 +0300

    fix new release

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -1,42 +1,43 @@
 param (
-	[Parameter()]
-	[ValidateNotNullOrEmpty()]
-	[string]
-	$OutputPath = '.\bin\v2rayN'
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $OutputPath = '.\bin\v2rayN'
 )
 
 Write-Host 'Building'
 
 dotnet publish `
-	.\v2rayN\v2rayN.csproj `
-	-c Release `
-	-r win-x64 `
-	--self-contained false `
-	-p:PublishReadyToRun=false `
-	-p:PublishSingleFile=true `
-	-o "$OutputPath\win-x64"
+    .\v2rayN\v2rayN.csproj `
+    -c Release `
+    -r win-x64 `
+    --self-contained false `
+    -p:PublishReadyToRun=false `
+    -p:PublishSingleFile=true `
+    -o "$OutputPath\win-x64"
 
 dotnet publish `
-	.\v2rayN.Desktop\v2rayN.Desktop.csproj `
-	-c Release `
-	-r linux-x64 `
-	--self-contained true `
-	-p:PublishReadyToRun=false `
-	-p:PublishSingleFile=true `
-	-o "$OutputPath\linux-x64"
+    .\v2rayN.Desktop\v2rayN.Desktop.csproj `
+    -c Release `
+    -r linux-x64 `
+    --self-contained true `
+    -p:PublishReadyToRun=false `
+    -p:PublishSingleFile=true `
+    -o "$OutputPath\linux-x64"
 
+if (-Not $?) {
+    exit $lastExitCode
+}
 
-if ( -Not $? ) {
-	exit $lastExitCode
-	}
-
-if ( Test-Path -Path .\bin\v2rayN ) {
+if (Test-Path -Path .\bin\v2rayN) {
     rm -Force "$OutputPath\win-x64\*.pdb"
     rm -Force "$OutputPath\linux-x64\*.pdb"
 }
 
 Write-Host 'Build done'
 
-ls $OutputPath
-7z a  v2rayN.zip $OutputPath
+# Создаем два отдельных архива
+7z a "$OutputPath\v2rayN-windows-64.zip" "$OutputPath\win-x64\*"
+7z a "$OutputPath\v2rayN-linux-64.zip" "$OutputPath\linux-x64\*"
+
 exit 0
